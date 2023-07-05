05480b5e716c06a8799941e65820e38e1209b574
Firdaus Ismail <44713893+firdisml@users.noreply.github.com>
2023-07-05 22:59:27 +0800

    Main Page Enhancement

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "~/utils/api";
-
+import { AiFillQuestionCircle } from "react-icons/ai";
 const products = [
   {
     id: 1,
@@ -67,10 +67,124 @@ export default function Home() {
 
   return (
     <>
+
       <div className="bg-white">
+        <div className=" pt-8 sm:pb-1 lg:max-w-7xl lg:mx-auto lg:px-8">
+          <div className="px-4 flex items-center justify-between sm:px-6 lg:px-0">
+            <h2 className="flex text-xl font-semibold text-gray-900">Trending Now<span className="ml-2 mt-1"><AiFillQuestionCircle/></span></h2>
+          </div>
+
+          <div className="mt-8 relative">
+            <div className="relative w-full pb-6 -mb-6 overflow-x-auto">
+              <ul
+                role="list"
+                className="mx-4 inline-flex space-x-8 sm:mx-6 lg:mx-0 lg:space-x-0 lg:grid lg:grid-cols-4 lg:gap-x-8"
+              >
+                {getAllShoe.isFetching ? (<>
+                  <li className="w-64 inline-flex flex-col text-center lg:w-auto">
+                    <div className="group relative">
+                      <div className="w-full bg-gray-200 rounded-md overflow-hidden aspect-w-1 aspect-h-1">
+                        <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                          <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                        </svg>
+                      </div>
+                      <div className="mt-6">
+
+                        <h3 className="mt-1 font-semibold text-gray-900">
+                          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                        </h3>
+                        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="w-64 inline-flex flex-col text-center lg:w-auto">
+                    <div className="group relative">
+                      <div className="w-full bg-gray-200 rounded-md overflow-hidden aspect-w-1 aspect-h-1">
+                        <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                          <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                        </svg>
+                      </div>
+                      <div className="mt-6">
+
+                        <h3 className="mt-1 font-semibold text-gray-900">
+                          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                        </h3>
+                        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="w-64 inline-flex flex-col text-center lg:w-auto">
+                    <div className="group relative">
+                      <div className="w-full bg-gray-200 rounded-md overflow-hidden aspect-w-1 aspect-h-1">
+                        <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                          <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                        </svg>
+                      </div>
+                      <div className="mt-6">
+
+                        <h3 className="mt-1 font-semibold text-gray-900">
+                          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                        </h3>
+                        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="w-64 inline-flex flex-col text-center lg:w-auto">
+                    <div className="group relative">
+                      <div className="w-full bg-gray-200 rounded-md overflow-hidden aspect-w-1 aspect-h-1">
+                        <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                          <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                        </svg>
+                      </div>
+                      <div className="mt-6">
+
+                        <h3 className="mt-1 font-semibold text-gray-900">
+                          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                        </h3>
+                        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                      </div>
+                    </div>
+                  </li>
+                </>) : (<>{getAllShoe?.data?.map((product) => (
+                  <li key={product.id} className="w-64 inline-flex flex-col text-center lg:w-auto">
+                    <div className="group relative">
+                      <div className="w-full bg-gray-200 rounded-md overflow-hidden aspect-w-1 aspect-h-1">
+                        <img
+                          src={product?.pictures[0]}
+                          className="w-full h-full object-center object-cover group-hover:opacity-75"
+                        />
+                      </div>
+                      <div className="mt-6">
+
+                        <h3 className="mt-1 font-semibold text-gray-900">
+                          <Link href={product?.uniqueName}>
+                            <span className="absolute inset-0" />
+                            {product?.model?.brand?.name?.concat(" ", product?.name)}
+                          </Link>
+                        </h3>
+                        <p className="text-sm text-gray-500">{product.color}</p>
+                        <span className="inline-flex items-center rounded-full bg-indigo-100 px-3 py-0.5 text-sm font-medium text-indigo-800 mt-1">
+                          <svg className="-ml-1 mr-1.5 h-2 w-2 text-indigo-400" fill="currentColor" viewBox="0 0 8 8">
+                            <circle cx={4} cy={4} r={3} />
+                          </svg>
+                          {product?.status ? "Contains Pigskin" : "No Pigskin"}
+                        </span>
+                      </div>
+                    </div>
+                  </li>
+                ))}</>)}
+              </ul>
+            </div>
+          </div>
+        </div>
+
         <div className="py-8 sm:py-8 lg:max-w-7xl lg:mx-auto lg:px-8">
           <div className="px-4 flex items-center justify-between sm:px-6 lg:px-0">
-            <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Trending products</h2>
+            <h2 className="text-xl flex font-semibold text-gray-900">Newly Added<span className="ml-2 mt-1"><AiFillQuestionCircle/></span></h2>
           </div>
 
           <div className="mt-8 relative">
