e8579830561c95a7ed96c77acaedcb5cb19d87e2
Firdaus Ismail <44713893+firdisml@users.noreply.github.com>
2023-07-08 02:25:55 +0800

    Main Page Enhancement

--- src/pages/[id]/index.tsx
+++ src/pages/[id]/index.tsx
@@ -428,82 +428,78 @@ export default function Example() {
 
 
             <Dialog open={open} as="div" className="fixed z-10 inset-0 overflow-y-auto" initialFocus={cancelButtonRef} onClose={setOpen}>
-              <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+              <form onSubmit={handle_add_rating}>
+                <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                  <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                  <div className="relative w-full inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                    <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                      <Dialog.Title as="h3" className="text-lg font-medium text-gray-900 mb-5">
+                        Add Review
+                      </Dialog.Title>
+                      <div>
+                        <div className="mb-5">
+                          <label className="text-md font-semibold text-gray-500">Review Status</label>
+                          <select
+                            id="tabs"
+                            name="tabs"
+                            className="block w-full text-sm rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 mt-2"
+                            defaultValue={1}
+                            required
+                            onChange={(e) => { set_review_status(Boolean(parseInt(e.currentTarget.value))) }}
+                          >
+                            <option value={1}>Agree</option>
+                            <option value={0}>Disagree</option>
+                          </select>
+                        </div>
 
-                <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-
-                {/* This element is to trick the browser into centering the modal contents. */}
-                <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
-                  &#8203;
-                </span>
-                <div className="relative w-full inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    <Dialog.Title as="h3" className="text-lg font-medium text-gray-900 mb-5">
-                      Add Review
-                    </Dialog.Title>
-                    <div>
-                      <div className="mb-5">
-                        <label className="text-md font-semibold text-gray-500">Review Status</label>
-                        <select
-                          id="tabs"
-                          name="tabs"
-                          className="block w-full text-sm rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 mt-2"
-                          defaultValue={1}
-                          onChange={(e) => { set_review_status(Boolean(parseInt(e.currentTarget.value))) }}
-                        >
-                          <option value={1}>Agree</option>
-                          <option value={0}>Disagree</option>
-                        </select>
+                        <div className="mb-5">
+                          <label className="text-md font-semibold text-gray-500">Review Title</label>
+                          <input
+                            type="text"
+                            name="title"
+                            id="title"
+                            required
+                            className="focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md mt-2"
+                            placeholder="Review Title"
+                            onChange={(e) => set_title(e.currentTarget.value)}
+                            aria-describedby="email-optional"
+                          />
+                        </div>
                       </div>
-
-                      <div className="mb-5">
-                        <label className="text-md font-semibold text-gray-500">Review Title</label>
-                        <input
-                          type="email"
-                          name="email"
-                          id="email"
+                      <div>
+                        <label className="text-md font-semibold text-gray-500">Review Body</label>
+                        <textarea
+                          placeholder="Review Body"
+                          name="body"
+                          id="body"
                           required
+                          onChange={(e) => set_body(e.currentTarget.value)}
                           className="focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md mt-2"
-                          placeholder="Review Title"
-                          onChange={(e) => set_title(e.currentTarget.value)}
-                          aria-describedby="email-optional"
+                          defaultValue={''}
                         />
                       </div>
+                      <div>
+                      </div>
                     </div>
-                    <div>
-                      <label className="text-md font-semibold text-gray-500">Review Body</label>
-                      <textarea
-                        placeholder="Review Body"
-                        name="comment"
-                        id="comment"
-                        required
-                        onChange={(e) => set_body(e.currentTarget.value)}
-                        className="focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md mt-2"
-                        defaultValue={''}
-                      />
+                    <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                      <button
+                        type="submit"
+                        className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                      >
+                        Submit
+                      </button>
+                      <button
+                        type="button"
+                        className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                        onClick={() => setOpen(false)}
+                        ref={cancelButtonRef}
+                      >
+                        Cancel
+                      </button>
                     </div>
-                    <div>
-                    </div>
-                  </div>
-                  <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <button
-                      type="button"
-                      className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-                      onClick={handle_add_rating}
-                    >
-                      Submit
-                    </button>
-                    <button
-                      type="button"
-                      className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                      onClick={() => setOpen(false)}
-                      ref={cancelButtonRef}
-                    >
-                      Cancel
-                    </button>
                   </div>
                 </div>
-              </div>
+              </form>
             </Dialog>
 
 
