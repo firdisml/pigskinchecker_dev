affff0d494c5e24fddc497b0476aacff67829e7e
Firdaus Ismail <44713893+firdisml@users.noreply.github.com>
2023-07-07 18:15:34 +0800

    Main Page Enhancement

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,22 +3,25 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    const seed = await prisma.shoe.create({
+    const seed = await prisma.shoe.update({
+        where: {
+            uniqueName: "adidas-samba-decon-core-black-cloud-white-gum",
+        },
         data: {
-            name: "Samba OG Sporty & Rich",
-            uniqueName: "adidas-samba-og-sporty-rich-cloud-white-green-gum",
-            searchParameter: "adidassambaogsportyrichcloudwhitegreengumhq6075",
-            sku: "HQ6075",
-            color: "Cloud White / Green / Gum",
-            pictures: ["https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/ed70b56fe9b24738b626af060183eb8d_9366/Samba_OG_Sporty_and_Rich_Shoes_White_HQ6075_01_standard.jpg","https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/32408964108c4428a05daf060188882f_9366/Samba_OG_Sporty_and_Rich_Shoes_White_HQ6075_41_detail.jpg","https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/10b265376f2d47a6945eaf060188919e_9366/Samba_OG_Sporty_and_Rich_Shoes_White_HQ6075_42_detail.jpg","https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/840e0ae4fd4440459261af0601887543_9366/Samba_OG_Sporty_and_Rich_Shoes_White_HQ6075_05_standard.jpg"],
-            features: ["Upper leather", "round toe", "logo patch at the tongue", "signature 3-Stripes logo", "logo print to the side"],
-            sources: ["https://www.farfetch.com/my/shopping/men/adidas-samba-vegan-white-gum-sneakers-item-19517753.aspx"],
-            sizing: ["True To Size"],
-            description: "First released in 1950, adidas' Samba sneakers were designed d to provide footballers with grip and stability. This iteration is presented in white leather with green signature serrated 3-Stripes to the sides, while sat atop the traditional gum rubber outsole.",
-            modelId: "cljlfb0gd0001v4qonm6qkj6z",
+            name: "Samba Decon",
+            uniqueName: "adidas-samba-decon-core-black-cloud-white-gum",
+            searchParameter: "adidassambadeconcoreblackcloudwhitegumhqif0641",
+            sku: "IF0641",
+            color: "Core Black / Core White / Gum",
+            pictures: ["https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/883a6bdaeb7b4e708219afd2010a0f52_9366/Samba_Decon_Shoes_Black_IF0641_01_standard.jpg", "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/14e2932de63546a19018afd2010a4cb6_9366/Samba_Decon_Shoes_Black_IF0641_41_detail.jpg", "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/5d47116a679c434285eaafd2010a553d_9366/Samba_Decon_Shoes_Black_IF0641_42_detail.jpg", "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/0cd0b30cdaca4746b624afd2010a41bd_9366/Samba_Decon_Shoes_Black_IF0641_05_standard.jpg"],
+            features: ["Regular fit", "Lace closure", "Leather upper", "Leather lining", "Rubber outsole"],
+            sources: ["https://www.adidas.com.my/en/samba-decon-shoes/IF0641.html"],
+            sizing: ["Regular Fit"],
+            description: "The adidas Samba shoes take on a deconstructed aesthetic without missing a beat in style. These shoes reflect a rich legacy with their timeless design. From the feet of football athletes to the high-fashion runways, the adidas Samba shoes embody self-expression and effortless style.",
+            modelId: "cljseu0ps0001v4fgdhrbm5k2",
             status: false
         }
-    })
+    }) 
 
     console.log(seed)
 }
