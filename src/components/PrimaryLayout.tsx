5957e1a977f47191d358657c932badcc6b2bfeb2
Firdaus Ismail <44713893+firdisml@users.noreply.github.com>
2023-07-08 22:17:51 +0800

    Main Page Enhancement

--- src/components/PrimaryLayout.tsx
+++ src/components/PrimaryLayout.tsx
@@ -116,8 +116,98 @@ export default function PrimaryLayout(props: {
       ]
     return (
         <>
+            <div className="min-h-full">
+                <Disclosure as="nav" className="bg-white dark:bg-gray-900 border-b border-gray-300 dark:border-gray-700">
+                    {({ open }) => (
+                        <>
+                            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                                <div className="flex items-center justify-between h-16">
+                                    <div className="items-center">
+                                        <div className="flex flex-shrink-0">
+                                            <Link href="/" className="text-gray-600 font-bold dark:text-white mb-1.5 lg:text-3xl">
+                                                <h1 className={orbitron.className} ><a className="tracking-wider text-xl">SOLEBITE</a></h1>
 
-            <Transition.Root show={dash} as={Fragment}>
+                                            </Link>
+
+                                        </div>
+
+                                    </div>
+
+                                    <div className="hidden md:block">
+                                        <div className="ml-10 flex items-baseline space-x-4">
+                                            <button onClick={(e) => dash ? setDash(false) : setDash(true)} className="flex text-gray-600 dark:text-white bg-transparent hover:bg-opacity-75 px-3 py-2 rounded-md text-sm font-semibold">
+                                                <HiSearch className="h-5 w-5 mr-2" aria-hidden="true" /> Search
+                                            </button>
+
+                                            {sessionData ?
+                                                <button
+                                                    onClick={() => void signOut()}
+                                                    className="flex text-gray-600 dark:text-white bg-transparent hover:bg-opacity-75 px-3 py-2 rounded-md text-sm font-semibold"
+                                                >
+                                                    <HiUser className="h-5 w-5 mr-2" /> {capitalCase(String(sessionData?.user?.name))}
+                                                </button>
+
+
+                                                :
+
+                                                <button
+                                                    onClick={() => void signIn()}
+                                                    className="flex text-gray-600 dark:text-white bg-transparent hover:bg-opacity-75 px-3 py-2 rounded-md text-sm font-semibold"
+                                                >
+                                                    <HiOutlineSquaresPlus className="h-5 w-5 mr-2" /> Login
+                                                </button>}
+
+
+                                        </div>
+                                    </div>
+                                    <div className="-mr-2 flex md:hidden">
+                                        {/* Mobile menu button */}
+                                        <button onClick={(e) => dash ? setDash(false) : setDash(true)} className="flex text-gray-600 dark:text-white bg-transparent hover:bg-opacity-75 px-3 py-2 rounded-md text-sm font-semibold">
+                                            <HiSearch className="h-6 w-6 mr-5" aria-hidden="true" />
+                                        </button>
+
+                                        <Disclosure.Button
+                                            className="bg-transparent inline-flex items-center justify-center p-2 text-gray-700 dark:text-white">
+                                            {open ? (
+                                                <XIcon className="block h-6 w-6" aria-hidden="true" />
+                                            ) : (
+                                                <MenuIcon
+                                                    className="block h-6 w-6"
+                                                    aria-hidden="true"
+                                                />
+                                            )}
+                                        </Disclosure.Button>
+
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <Disclosure.Panel className="md:hidden">
+                                <div className="px-2 pt-2 pb-3 space-y-3 sm:px-3">
+                                    {router.route.includes("/submit") ? (<Disclosure.Button
+                                        onClick={() => { router.push('/') }}
+                                        as="a"
+                                        className="flex dark:text-white text-gray-700 bg-transparent hover:bg-opacity-75 px-3 py-3 rounded-md text-sm font-semibold"
+                                        aria-current="page"
+                                    >
+                                        <HiOutlineSquaresPlus className="h-5 w-5 mr-2" /> Browse Promotion
+                                    </Disclosure.Button>) : (<Disclosure.Button
+                                        onClick={() => { void router.push('/submit') }}
+                                        as="a"
+                                        className="flex dark:text-white text-gray-700 bg-transparent  px-3 py-3 rounded-md text-sm font-semibold"
+                                        aria-current="page"
+                                    >
+                                        <HiOutlineSquaresPlus className="h-5 w-5 mr-2" /> Submit Promotion
+                                    </Disclosure.Button>)}
+
+
+                                </div>
+                            </Disclosure.Panel>
+                        </>
+                    )}
+                </Disclosure>
+                <Transition.Root show={dash} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={setDash}>
         <div className="fixed inset-0" />
 
@@ -357,7 +447,7 @@ export default function PrimaryLayout(props: {
                         {props.children}
                     </div>
                 </main>
-         
+            </div>
         </>
     );
 }
