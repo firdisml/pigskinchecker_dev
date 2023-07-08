077e54edc5ba5c4852e75aac0d34afc6faaf9262
Firdaus Ismail <44713893+firdisml@users.noreply.github.com>
2023-07-09 02:47:29 +0800

    Main Page Enhancement

--- src/components/PrimaryLayout.tsx
+++ src/components/PrimaryLayout.tsx
@@ -216,45 +216,16 @@ export default function PrimaryLayout(props: {
 
 
 
-                <Transition.Root show={dash} as={Fragment}>
-      <Dialog as="div" className="relative z-10" id="scrollable" onClose={setDash}>
-        <div className="fixed inset-0" />
+
+      <Dialog as="div" open={dash} className="relative z-10" id="scrollable" onClose={setDash}>
+      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
 
         <div className="fixed inset-0 overflow-hidden">
           <div className="absolute inset-0 overflow-hidden">
             <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full">
-              <Transition.Child
-                as={Fragment}
-                enter="transform transition ease-in-out duration-500 sm:duration-700"
-                enterFrom="translate-x-full"
-                enterTo="translate-x-0"
-                leave="transform transition ease-in-out duration-500 sm:duration-700"
-                leaveFrom="translate-x-0"
-                leaveTo="translate-x-full"
-              >
-                <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
+                <Dialog.Panel className="pointer-events-auto w-screen max-w-lg">
                   <form className="flex h-full flex-col divide-y divide-gray-200 bg-white shadow-xl">
                     <div className="h-0 flex-1 overflow-y-auto">
-                      <div className="bg-indigo-700 py-6 px-4 sm:px-6">
-                        <div className="flex items-center justify-between">
-                          <Dialog.Title className="text-lg font-medium text-white">New Project</Dialog.Title>
-                          <div className="ml-3 flex h-7 items-center">
-                            <button
-                              type="button"
-                              className="rounded-md bg-indigo-700 text-indigo-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
-                              onClick={() => setDash(false)}
-                            >
-                              <span className="sr-only">Close panel</span>
-                              X
-                            </button>
-                          </div>
-                        </div>
-                        <div className="mt-1">
-                          <p className="text-sm text-indigo-300">
-                            Get started by filling in the information below to create your new project.
-                          </p>
-                        </div>
-                      </div>
                       <div className="flex flex-1 flex-col justify-between">
                         <div className="divide-y divide-gray-200 px-4 sm:px-6">
                           <div className="space-y-6 pt-6 pb-5">
@@ -413,12 +384,10 @@ export default function PrimaryLayout(props: {
                     </div>
                   </form>
                 </Dialog.Panel>
-              </Transition.Child>
             </div>
           </div>
         </div>
       </Dialog>
-    </Transition.Root>
                 <main>
                     <div className="max-w-7xl mx-auto">
                         {props.children}
