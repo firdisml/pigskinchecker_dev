20260da97211cde63a26b26266b826252cf34b21
Firdaus Ismail <44713893+firdisml@users.noreply.github.com>
2023-07-09 11:45:29 +0800

    Main Page Enhancement

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en" className="h-full">
-      <Head />
+      <Head><meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1"/></Head>
       <body className="h-full bg-white dark:bg-gray-800">
         <Main />
         <NextScript />
